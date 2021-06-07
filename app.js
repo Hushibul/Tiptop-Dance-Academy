99a630c745d2e60174e98b7191d851ece3671022
Hushibul <nayeemninz786@gmail.com>
2021-06-07 11:23:05 +0600

    app.get /contact added

--- app.js
+++ app.js
@@ -31,6 +31,10 @@ app.get('/', (req, res)=>{
     const params = { };
     res.status(200).render('home.pug', params);
 })
+app.get('/contact', (req, res)=>{
+    const params = { };
+    res.status(200).render('contact.pug', params);
+})
 app.post('/contact', (req, res)=>{
     var myData = new Contact(req.body);
     myData.save().then( ()=>{
